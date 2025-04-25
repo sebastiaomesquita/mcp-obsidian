04a96c8eb02e04654bc88753d018ad22fcdfb63b
gabrieljablonski <gabriel.g.jablonski@gmail.com>
2025-04-25 19:37:43 -0300

    fix: non-optional `newLeaf` param for `open_file` tool

--- src/obsidian/index.ts
+++ src/obsidian/index.ts
@@ -140,7 +140,7 @@ export class Obsidian {
     });
   }
 
-  openFile({ filename, newLeaf }: { filename: string; newLeaf?: boolean }) {
+  openFile({ filename, newLeaf }: { filename: string; newLeaf: boolean }) {
     const qs = newLeaf ? "?newLeaf=true" : "";
     return this.fetch<void>(`/open/${sanitizeAndEncodePath(filename)}${qs}`, {
       method: "POST",
